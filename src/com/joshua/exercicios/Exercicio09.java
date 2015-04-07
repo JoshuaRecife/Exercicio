bd771cb00b17404106a58a4475952fa0ae45b169
Joshua S. <joshuarecife@gmail.com>
2015-04-06 21:37:47 -0300

    Apenas os números impares entre 1 e 50

---
+++ src/com/joshua/exercicios/Exercicio09.java
@@ -0,0 +1,15 @@
+package com.joshua.exercicios;
+
+public class Exercicio09 {
+
+	public static void main(String[] args) {
+		//09
+		/* Faça um programa que imprima na tela apenas os números impares entre 1 e 50 */
+
+		for(int i = 1;i <= 50;i++){
+			if(i % 2 == 1){
+				System.out.println(i);
+			}
+		}
+	}
+}
