5b191d66c5cd918afa3143b674277a09b4c1ca15
Joshua S. <joshuarecife@gmail.com>
2015-04-16 15:51:03 -0300

    Faça um programa que calcule o valor de H com N termos.

--- src/com/joshua/exercicios/Exercicio33.java
+++ src/com/joshua/exercicios/Exercicio33.java
@@ -3,8 +3,9 @@ package com.joshua.exercicios;
 public class Exercicio33 {
 
 	public static void main(String[] args) {
-//		Sendo H = 1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/6 + 1/7 + 1/8 + 1/9 Faça um programa
-//		que calcule o valor de H com N termos e mostre assim: A soma de 'H' é = 9
+//		Faça um programa que calcule o valor de H com N termos e mostre assim: 
+//		H = 1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/6 + 1/7 + 1/8 + 1/9
+//		A soma de 'H' é = 9
 		int soma=1;
 		System.out.print("H = ");
 		System.out.print("1");
@@ -15,7 +16,5 @@ public class Exercicio33 {
 		}
 		System.out.println("");
 		System.out.print("A soma de 'H' é = "+(soma));
-
 	}
-
 }
