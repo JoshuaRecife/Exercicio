d171184e17d7fff174b78ce4e78b17d1ca7f8c42
Joshua S. <joshuarecife@gmail.com>
2015-04-21 18:10:11 -0300

    Criar 3 vetores A, B e C cada um com 10 elementos inteiros...

--- src/com/joshua/cursojava/arrays/ExercicioArrays07.java
+++ src/com/joshua/cursojava/arrays/ExercicioArrays07.java
@@ -12,7 +12,7 @@ public class ExercicioArrays07 {
 		int[] vetorA = new int[10];
 		int[] vetorB = new int[vetorA.length];
 		int[] vetorC = new int[vetorA.length];
-		
+
 		for(int i=0;i<vetorA.length;i++){
 			System.out.println("Digite o valor do vetor A da posição: "+i);
 			vetorA[i] = scan.nextInt();
