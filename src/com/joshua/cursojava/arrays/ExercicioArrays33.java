299b2df455ef5abd99b0917239ce30931ca73714
Joshua S. <joshuarecife@gmail.com>
2015-05-12 11:24:20 -0300

    Relação de todos os pares de 0 até o respectivo elemento.

--- src/com/joshua/cursojava/arrays/ExercicioArrays33.java
+++ src/com/joshua/cursojava/arrays/ExercicioArrays33.java
@@ -8,15 +8,14 @@ public class ExercicioArrays33 {
 //	33) Criar um vetor A com 10 elementos inteiros. Escreva um programa que
 //		imprima cada elemento do vetor A e a relação de todos os pares de 0
 //		até o respectivo elemento.	
-		
 		Scanner scan = new Scanner(System.in);
 		int[] vetorA = new int[10];
 		
-		for(int i = 0;i<vetorA.length;i++){
+		for(int i=0; i<vetorA.length; i++){
 			System.out.println("Digite o valor da posição: "+i);
 			vetorA[i] = scan.nextInt();
 		}
-		for(int i = 0;i<vetorA.length;i++){
+		for(int i=0; i<vetorA.length; i++){
 			System.out.println("Analisando o número: "+vetorA[i]);
 			
 			for(int j = 2;j<vetorA[i];j++){
